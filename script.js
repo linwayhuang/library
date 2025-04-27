b4e9c7a5c3398d9dbe064e46d8a2f3b7a1ea18c6
Jackson Bond <111647134+linwayhuang@users.noreply.github.com>
2025-04-27 12:25:48 -0500

    Setup all 3 files" git status

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    if (this.read == 'yes') {
+        this.info = function() {
+            console.log('The ' + this.title + ' by ' + this.author + ', ' + this.pages + ' pages, already read.')
+        }
+    } else if (this.read == 'no') {
+        this.info = function() {
+            console.log('The ' + this.title + ' by ' + this.author + ', ' + this.pages + ' pages, not read yet.')
+        }
+    }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
+    book = new Book(title, author, pages, read);
+    book.id = crypto.randomUUID();
+    myLibrary.push(book);
 }
