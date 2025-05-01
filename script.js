4c8cc962928b54fe3d9b8adf5c127e7084a12f98
Jackson Bond <111647134+linwayhuang@users.noreply.github.com>
2025-04-30 20:17:23 -0500

    Modified the addEListenerToStatusButton by adding event delegation

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-const buttonStatus = document.querySelectorAll('.button-status') /* Select multiple buttons */
-
-buttonStatus.forEach(button => { /* Add event listener for multiple buttons */
-  button.addEventListener('click', function() {
-    button.classList.toggle('inactive');
-    if (button.classList.contains('inactive')) {
-      button.textContent = 'Not Read';
-    } else {
-      button.textContent = 'Read';
-    }
-  })
-})
-
+addEListenerToStatusButton();
 
 const myLibrary = [];
 
@@ -26,33 +14,54 @@ function Book(title, author, pages, status) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    if (this.status == 'yes') {
-        this.info = function() {
-            console.log('The ' + this.title + ' by ' + this.author + ', ' + this.pages + ' pages, already read.')
-        }
-    } else if (this.status == 'no') {
-        this.info = function() {
-            console.log('The ' + this.title + ' by ' + this.author + ', ' + this.pages + ' pages, not read yet.')
-        }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  if (this.status == 'yes') {
+    this.info = function() {
+      console.log('The ' + this.title + ' by ' + this.author + ', ' + this.pages + ' pages, already read.')
     }
+  } else if (this.status == 'no') {
+    this.info = function() {
+      console.log('The ' + this.title + ' by ' + this.author + ', ' + this.pages + ' pages, not read yet.')
+    }
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
-    const book = new Book(title, author, pages, read);
-    book.id = crypto.randomUUID();
-    myLibrary.push(book);
+  const book = new Book(title, author, pages, read);
+  book.id = crypto.randomUUID();
+  myLibrary.push(book);
+}
+
+function addEListenerToStatusButton() {
+
+  // Delegate the event listener to the container of the buttons
+  // Event delegation doesn't work if you delegate to a class. Needs to be a container
+  const tbody = document.querySelector('tbody');
+
+  // Add event listener for multiple buttons by using event delegation
+  // This will automatically add event listener to dynamically created buttons
+    tbody.addEventListener('click', function(e) {
+      const event = e.target; // 'target' is the property of the dispatch element
+      if (event.matches('.button-status')) { //'matches' helps to access the class
+        event.classList.toggle('inactive'); // 'classList' helps to manipulate the element's classes
+        if (event.classList.contains('inactive')) {
+          event.textContent = 'Not Read';
+        } else {
+          event.textContent = 'Read';
+        }
+      }
+    })
 }
 
 function displayBookInTable() {
 
 }
 
-function displayBookPropertiesInCell (book) {
+function displayBookPropertiesInCell(book) {
   const tbody = document.querySelector('tbody');
 
   // Clear existing table content
@@ -90,6 +99,7 @@ function displayBookPropertiesInCell (book) {
     }
     row.appendChild(cell);
   })
+
   // Create the buttonRemove
   const cell = document.createElement('td');
   cell.classList.add('table-remove-book');
@@ -98,6 +108,7 @@ function displayBookPropertiesInCell (book) {
   cell.appendChild(buttonRemove);
   row.appendChild(cell);
 
-  // Append all to the table
+  // Append all elements to the table
   tbody.appendChild(row);
+  // addEListenerToStatusButton(); // Do this to add event listener every time a new button is created
 }
